b42026de556e7a84adc6d0e7df8164573caa59db
Ubuntu <ubuntu@ip-172-31-3-196.us-east-2.compute.internal>
2022-07-17 17:28:34 +0000

    update

--- components/createPdf.js
+++ components/createPdf.js
@@ -125,7 +125,27 @@ const createPdf = async (input, body, res) => {
 			box88,
 			box85,
 		} = body;
+			box14=isNaN(parseInt(box14))?0:parseInt(box14);
+			box16=isNaN(parseInt(box16))?0:parseInt(box16);
 
+			field83=isNaN(parseInt(field83))?0:parseInt(field83);
+			box18=isNaN(parseInt(box18))?0:parseInt(box18);
+			box55=isNaN(parseInt(box55))?0:parseInt(box55);
+			box20=isNaN(parseInt(box20))?0:parseInt(box20);
+			box75=isNaN(parseInt(box75))?0:parseInt(box75);
+			box22=isNaN(parseInt(box22))?0:parseInt(box22);
+			box42=isNaN(parseInt(box42))?0:parseInt(box42);
+			box43=isNaN(parseInt(box43))?0:parseInt(box43);
+			box44=isNaN(parseInt(box44))?0:parseInt(box44);
+			box52=isNaN(parseInt(box52))?0:parseInt(box52);
+			box66=isNaN(parseInt(box66))?0:parseInt(box66);
+			box39=isNaN(parseInt(box39))?0:parseInt(box39);
+			box41=isNaN(parseInt(box41))?0:parseInt(box41);
+			box77=isNaN(parseInt(box77))?0:parseInt(box77);
+			box87=isNaN(parseInt(box87))?0:parseInt(box87);
+			box88=isNaN(parseInt(box88))?0:parseInt(box88);
+			box85=isNaN(parseInt(box85))?0:parseInt(box85);
+	
 		// Modify doc, fill out the ..
 		const fieldNames = pdfDoc.getForm().getFields();
 
@@ -263,13 +283,14 @@ const createPdf = async (input, body, res) => {
 				if (foreignPropertyCheck === false) field.check();
 			}
 			if (i == 45) {
-				field.setText(box14);
+console.log(typeof box14)
+				field.setText(box14.toFixed(2).toString()+' ');
 			}
 			if (i == 47) {
-				field.setText(box42);
+				field.setText(box42.toFixed(2).toString()+' ');
 			}
 			if (i == 71) {
-				field.setText(box66);
+				field.setText(box66.toFixed(2).toString()+' ');
 			}
 
 			const fieldes18Sum =
@@ -289,90 +310,90 @@ const createPdf = async (input, body, res) => {
 				// field14 +
 				// field15 +
 				// field16 +
-				Number(box66);
+				Number(box66||'0');
 			// field18;
 			if (i == 73) {
-				field.setText(fieldes18Sum.toString());
+				field.setText(fieldes18Sum.toFixed(2).toString()+' ');
 			}
 			if (i == 86) {
-				field.setText(fieldes18Sum.toString());
+				field.setText(fieldes18Sum.toFixed(2).toString()+' ');
 			}
 			if (i == 92) {
-				field.setText(fieldes18Sum.toString());
+				field.setText(fieldes18Sum.toFixed(2).toString()+' ');
 			}
 			if (i == 93) {
-				field.setText(fieldes18Sum.toString());
+				field.setText(fieldes18Sum.toFixed(2).toString()+' ');
 			}
 			if (i == 94) {
-				field.setText(box52);
+				field.setText(box52.toFixed(2).toString()+' ');
 			}
 
 			if (i == 95) {
-				field.setText(box20);
+				field.setText(box20.toFixed(2).toString()+' ');
 			}
 			if (i == 99) {
-				field.setText(box44);
+				field.setText(box44.toFixed(2).toString()+' ');
 			}
 			if (i == 110) {
 				if (box16 > 290.5) {
 					return res.status(400).send('it should be less than 290.5');
 				}
-				field.setText(box16);
+				field.setText(box16.toFixed(2).toString()+' ');
 			}
 			if (i == 112) {
-				field.setText(box77);
+				field.setText(box77.toFixed(2).toString()+' ');
 			}
 			const sum33to50 =
-				Number(box20) + Number(box44) + Number(box16) + Number(box77);
+				Number(box20||'0') + Number(box44||'0') + Number(box16||'0') + Number(box77||'0');
 			if (i == 117) {
-				field.setText(sum33to50.toString());
+				field.setText(sum33to50.toFixed(2).toString()+' ');
 			}
 			if (i == 118) {
-				field.setText(fieldes18Sum.toString());
+				field.setText(fieldes18Sum.toFixed(2).toString()+' ');
 			}
 			const NetIncome = fieldes18Sum - sum33to50;
 
 			if (i == 119) {
 				// check if net income is negative
 				if (NetIncome < 0) {
-					field.setText('0');
+					field.setText((0).toFixed(2).toString()+' ');
 				} else {
-					field.setText(NetIncome.toString());
+					field.setText(NetIncome.toFixed(2).toString()+' ');
 				}
 			}
 			if (i == 120) {
-				field.setText('0');
+				field.setText((0).toFixed(2).toString()+' ');
 			}
 			const totalNet = NetIncome - 0;
 			if (i == 121) {
 				// check if net income is negative
 				if (totalNet < 0) {
-					field.setText('0');
+					field.setText((0).toFixed(2).toString()+' ');
 				} else {
-					field.setText(totalNet.toString());
+					field.setText(totalNet.toFixed(2).toString()+' ');
 				}
 			}
 			if (i == 122) {
 				if (totalNet < 0) {
-					field.setText('0');
+					field.setText((0).toFixed(2).toString()+' ');
 				} else {
-					field.setText(totalNet.toString());
+					field.setText(totalNet.toFixed(2).toString()+' ');
 				}
 			}
 			if (i == 123) {
-				field.setText(box43);
+				field.setText(box43.toFixed(2).toString()+' ');
 			}
 			if (i == 124) {
-				field.setText(box39);
+				field.setText(box39.toFixed(2).toString()+' ');
 			}
 
-			const sum56to64 = Number(box43) + Number(box39);
+			const sum56to64 = Number(box43||'0') + Number(box39||'0');
 
 			if (i == 133) {
-				field.setText(sum56to64.toString());
+				field.setText(sum56to64.toFixed(2).toString()+' ');
 			}
 			if (i == 134) {
-				field.setText('0');
+				field.setText((0).toFixed(2).toString()+' ');
 			}
 			let taxable = totalNet - sum56to64;
 			if(!isNaN(taxable))line26000=taxable;
@@ -380,7 +401,7 @@ const createPdf = async (input, body, res) => {
 				taxable = 0;
 			}
 			if (i == 135) {
-				field.setText(taxable.toString());
+				field.setText(taxable.toFixed(2).toString()+' ');
 			}
 
 			// * Federal Tax First calculation
@@ -388,9 +409,9 @@ const createPdf = async (input, body, res) => {
 			if (taxable <= 49020) {
 				if (i == 136) {
 					if (taxable < 0) {
-						field.setText('0');
+						field.setText((0).toFixed(2).toString()+' ');
 					} else {
-						field.setText(taxable.toString());
+						field.setText(taxable.toFixed(2).toString()+' ');
 					}
 				}
 
@@ -399,26 +420,26 @@ const createPdf = async (input, body, res) => {
 					federalTaxA = 0;
 				}
 				if (i == 138) {
-					field.setText(taxable.toString());
+					field.setText(taxable.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA * 0.15;
 				federalTaxA = Math.round(federalTaxA * 100) / 100;
 				if (i == 140) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA + 0;
 				if (i == 142) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 				// * Federal Tax Second calculation
 			} else if (taxable > 49020 && taxable <= 98040) {
 				if (i == 143) {
 					if (taxable < 0) {
-						field.setText('0');
+						field.setText((0).toFixed(2).toString()+' ');
 					} else {
-						field.setText(taxable.toString());
+						field.setText(taxable.toFixed(2).toString()+' ');
 					}
 				}
 
@@ -427,19 +448,19 @@ const createPdf = async (input, body, res) => {
 					federalTaxA = 0;
 				}
 				if (i == 145) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA * 0.205;
 				// only 2 decimal places
 				federalTaxA = Math.round(federalTaxA * 100) / 100;
 				if (i == 147) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA + 7353;
 				if (i == 149) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 				// * Federal Tax Third calculation
 			} else if (taxable > 98040 && taxable <= 151978) {
@@ -447,7 +468,7 @@ const createPdf = async (input, body, res) => {
 					if (taxable < 0) {
 						field.setText('0');
 					} else {
-						field.setText(taxable.toString());
+						field.setText(taxable.toFixed(2).toString()+' ');
 					}
 				}
 
@@ -456,18 +477,18 @@ const createPdf = async (input, body, res) => {
 					federalTaxA = 0;
 				}
 				if (i == 152) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA * 0.26;
 				federalTaxA = Math.round(federalTaxA * 100) / 100;
 				if (i == 154) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA + 17402.1;
 				if (i == 156) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 				// * Federal Tax Fourth calculation
 			} else if (taxable > 151978 && taxable <= 216511) {
@@ -475,7 +496,7 @@ const createPdf = async (input, body, res) => {
 					if (taxable < 0) {
 						field.setText('0');
 					} else {
-						field.setText(taxable.toString());
+						field.setText(taxable.toFixed(2).toString()+' ');
 					}
 				}
 				console.log(taxable);
@@ -484,42 +505,42 @@ const createPdf = async (input, body, res) => {
 					federalTaxA = 0;
 				}
 				if (i == 159) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA * 0.29;
 				federalTaxA = Math.round(federalTaxA * 100) / 100;
 				if (i == 161) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 
 				federalTaxA = federalTaxA + 31425.98;
 				if (i == 163) {
-					field.setText(federalTaxA.toString());
+					field.setText(federalTaxA.toFixed(2).toString()+' ');
 				}
 			}
 
 			if (i == 181) {
-				field.setText(field83);
+				field.setText(field83.toFixed(2).toString()+' ');
 			}
 			if (i == 183) {
-				field.setText(box18);
+				field.setText(box18.toFixed(2).toString()+' ');
 			}
 			if (i == 252) {
-				field.setText(box22);
+				field.setText(box22.toFixed(2).toString()+' ');
 			}
 			// if (i == 183) {
-			// 	field.setText(box18);
+			// 	field.setText(box18.toFixed(2).toString()+' ');
 			// }
 
 			if (i == 185) {
-				field.setText(box87);
+				field.setText(box87.toFixed(2).toString()+' ');
 			}
 			if (i == 186) {
-				field.setText(box88);
+				field.setText(box88.toFixed(2).toString()+' ');
 			}
 			if (i == 205) {
-				field.setText(box85);
+				field.setText(box85.toFixed(2).toString()+' ');
 			}
 		});
 		const on428Form=await on428({line26000})
